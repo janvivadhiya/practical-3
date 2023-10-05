25472de2af95fa4664470514306d150956c4cab6
janvivadhiya <141604821+janvivadhiya@users.noreply.github.com>
2023-10-05 08:05:10 +0530

    Add files via upload

---
+++ pra3.5.cpp
@@ -0,0 +1,33 @@
+#include<iosteram>
+using namespace std;
+class distance
+{
+	private:
+		int feet;
+		float inches;
+	public:
+		void get_details()
+		{
+			cout<<"enter the value of feet ::";
+			cin>>feet;
+			
+			cout<<"enter the value of inches ::";
+			cin>>inches;
+		}
+		void display_details()
+		{
+			cout<<"the value of feet is   ::"<<feet<<endl;
+			cout<<"the value of inches is ::"<<inches<<endl;
+		}
+};
+int main()
+{
+	distance d1;
+	cout<<"vadhiya janvi c."<<endl;
+	cout<<"220130318031"<<endl;
+	
+	d1.get_details();
+	d1.display_details();
+	
+	return 0;
+}
